a484c9071f9a5956fed2246db269a292980ae2f8
smmmfrd <smmmfrd@gmail.com>
2023-02-21 17:32:01 -0800

    Remove Paste Detection, Add Item Modal and Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import { useEffect, useState } from "react";
+import { useRef } from "react";
 import EquipLayout from "./components/EquipLayout";
+import ItemImporter from "./components/ItemImporter";
 
 export default function App() {
-    useEffect(() => {
-        window.addEventListener("keydown", (e) => handleKeyDown(e));
-    }, []);
-
-    function handleKeyDown(event) {
-        if(event.key === "v" && (event.metaKey || event.ctrlKey)) {
-            console.log("get the paste!!!");
-        }
-    }
+    const itemImporter = useRef(null);
 
     return (
         <main className="mx-auto max-w-2xl pt-10 flex flex-col items-center gap-4">
-            <button className="btn btn-primary">Import Item (Ctrl + V)</button>
-
+            <button className="btn btn-primary" onClick={() => itemImporter.current.showModal()}>Import Item (Ctrl + V)</button>
+            <ItemImporter ref={itemImporter}/>
             <EquipLayout />
         </main>
     );
---
+++ src/components/ItemImporter.jsx
@@ -0,0 +1,14 @@
+import { forwardRef } from "react";
+
+const ItemImporter = forwardRef((props, ref) => {
+    return (
+        <dialog ref={ref} className="rounded-lg relative p-10">
+            <button className="btn btn-circle btn-outline btn-sm absolute top-1 left-1" onClick={() => ref.current.close()}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+            </button>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+        </dialog>
+    );
+});
+
+export default ItemImporter;
