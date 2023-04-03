2ba81d2ce4ab55bc9af478d80ecd59d63959735c
smmmfrd <smmmfrd@gmail.com>
2023-04-03 14:33:56 -0700

    Character Deletion and Character Select Styling

--- src/components/CharacterSelect.jsx
+++ src/components/CharacterSelect.jsx
@@ -31,6 +31,39 @@ export default function CharacterSelect({ characterChosen }) {
 		closeCreateMenu();
 	}
 
+	function deleteCharacter(e, name) {
+		e.stopPropagation();
+		console.log('Deleting: ', name);
+
+		const newList = characterList.filter(char => char !== name);
+		setCharacterList(newList);
+		localStorage.setItem("characters", JSON.stringify(newList));
+	}
+
+	function CharacterItem(char) {
+		return (
+			<li key={char} className="group btn w-max mx-auto">
+				<button
+					key={char}
+					onClick={() => characterChosen(char)}
+					className="font-semibold text-lg flex"
+				>
+					{char}
+					<div
+						className="group-hover:opacity-100 opacity-30 transition-opacity duration-200 divider divider-horizontal"
+					></div>
+					<span
+						className="group-hover:opacity-100 opacity-30 transition-opacity duration-200 btn btn-circle btn-outline btn-xs"
+						title="Delete this Character"
+						onClick={(e) => deleteCharacter(e, char)}
+					>
+						<svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+					</span>
+				</button>
+			</li>
+		)
+	}
+
 	return (
 		<>
 			<dialog ref={createMenu} className="rounded-2xl relative p-10">
@@ -61,24 +94,16 @@ export default function CharacterSelect({ characterChosen }) {
 					<button type="submit" className="btn">Create Character</button>
 				</form>
 			</dialog>
-			<h1>Pick a character</h1>
-			<button className="btn" onClick={openCreateMenu}>Make a New Character</button>
+			<button className="mt-8 btn" onClick={openCreateMenu}>Make a New Character</button>
 
 			{characterList.length > 0 ?
-				<ul className="flex flex-col gap-8 text-center">
-					{characterList.map(char => (
-						<li key={char}>
-							<button
-								key={char}
-								onClick={() => characterChosen(char)}
-								className="btn"
-							>
-								{char}
-							</button>
-						</li>
-					))}
-				</ul> :
-				<p className="text-neutral-content">No Characters</p>
+				<>
+					<h1 className="text-4xl underline">PICK A CHARACTER</h1>
+					<ul className="flex flex-col gap-8 text-center">
+						{characterList.map(CharacterItem)}
+					</ul>
+				</> :
+				<h2 className="text-neutral-content">No Characters</h2>
 			}
 		</>
 	)
