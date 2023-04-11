5d386cc4e4637470ca17d392ac7949554941c935
smmmfrd <smmmfrd@gmail.com>
2023-04-11 11:22:04 -0700

    Character Create Dialog Styling

--- src/components/CharacterSelect.jsx
+++ src/components/CharacterSelect.jsx
@@ -37,12 +37,16 @@ export default function CharacterSelect({ characterChosen }) {
 	}
 
 	function deleteCharacter(e, name) {
-		e.stopPropagation();
 		console.log('Deleting: ', name);
 
 		const newList = characterList.filter(char => char !== name);
 		setCharacterList(newList);
 		localStorage.setItem("characters", JSON.stringify(newList));
+	}
+
+	function confirmDelete(e, char) {
+		e.stopPropagation();
+
 	}
 
 	function CharacterItem(char) {
@@ -59,7 +63,7 @@ export default function CharacterSelect({ characterChosen }) {
 					<span
 						className="group-hover:opacity-100 opacity-30 transition-opacity duration-200 btn btn-circle btn-outline btn-error btn-xs"
 						title="Delete this Character"
-						onClick={(e) => deleteCharacter(e, char)}
+						onClick={(e) => confirmDelete(e, char)}
 					>
 						<svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 fill-current" fill="none" viewBox="0 0 24 24"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>
 					</span>
@@ -70,32 +74,39 @@ export default function CharacterSelect({ characterChosen }) {
 
 	return (
 		<>
-			<dialog ref={createMenu} className="rounded-2xl relative p-10">
+			<dialog ref={createMenu} className="rounded-2xl relative pt-4 pb-10 px-6">
 				{/* CLOSE BUTTON */}
-				<button className="btn btn-circle btn-outline btn-sm absolute top-1 left-1" onClick={closeCreateMenu}>
-					<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+				<button 
+					title="Cancel"
+					className="btn btn-circle btn-outline btn-sm absolute top-2 left-2" 
+					onClick={closeCreateMenu}>
+					<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
 				</button>
 
-				<form onSubmit={characterCreate}>
-					<h2>Make a Character</h2>
-					<div className="">
-						<label>
-							Name:
+				<form onSubmit={characterCreate} className="flex flex-col items-center">
+					<h2 className="text-base-content text-3xl underline bold mb-8">Add a Character</h2>
+						<label className="flex flex-col items-center">
+							<span className="block text-xl text-base-content">
+								Name:
+							</span>
 							<input
 								type="text"
 								ref={nameInput}
 								pattern="[a-zA-Z]{7,23}"
 								minLength={7}
 								maxLength={23}
-								placeholder="..."
+								placeholder=""
 								className="input peer"
 							/>
-							<p className="peer-invalid:visible invisible">
+							<p className="peer-invalid:visible invisible w-56 text-center text-warning pt-1 pb-4">
 								Please enter a valid name, 7-23 characters only.
 							</p>
 						</label>
-					</div>
-					<button type="submit" className="btn">Create Character</button>
+					<button 
+						title="Create"
+						type="submit" 
+						className="btn">
+						Create Character</button>
 				</form>
 			</dialog>
 			<button className="mt-8 btn" onClick={openCreateMenu}>Make a New Character</button>
