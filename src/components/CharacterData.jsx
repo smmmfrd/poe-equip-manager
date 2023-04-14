0c82edf3a16469c025c28d95dd033bb25017bd3f
smmmfrd <smmmfrd@gmail.com>
2023-04-14 11:41:29 -0700

    Character Data Summary Styling Update

--- src/components/CharacterData.jsx
+++ src/components/CharacterData.jsx
@@ -17,14 +17,14 @@ export default function CharacterData({ data }) {
 	}, { a: 0, c: 0, e: 0, d: 0 });
 
 	return (
-		<section className="w-11/12 p-4 bg-accent-focus rounded-lg text-primary flex justify-between">
+		<section className="w-11/12 py-4 px-8 bg-base-200 rounded-lg text-base-content flex justify-around">
 			<header>
 				<h2 className="text-4xl underline">{characterName}</h2>
 				<p>Created - {formatTimeAgo(dateCreated)}</p>
 			</header>
 			<div className="basis-1/4">
 				<h3 className="text-2xl underline">Total Cost</h3>
-				<div className="flex flex-wrap gap-2">
+				<div className="text-right grid grid-cols-2 gap-0 items-end">
 					{totalCost.a > 0 && <p>{totalCost.a} Alch</p>}
 					{totalCost.c > 0 && <p>{totalCost.c} Chaos</p>}
 					{totalCost.e > 0 && <p>{totalCost.e} Exalted</p>}
